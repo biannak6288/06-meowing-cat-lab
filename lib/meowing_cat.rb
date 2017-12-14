12b290dba53c29c7658fda60ffa3010f9c2cf8af
biannak6288 <biannak6288@hstat.org>
2017-12-14 23:51:54 +0000

    finish cat lab

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,7 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name #used to set and get the cat's name
+    def meow 
+        puts "meow!"#the sound the cat makes, which is a meowing sound
+    end
+end 
